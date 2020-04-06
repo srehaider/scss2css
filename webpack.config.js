b68726cd6424c194ac5ff26b26e1136b8db4dc54
Rehan Haider <rehanhaider.dev@gmail.com>
2020-04-06 15:48:51 +0500

    Updated dependencies to latest

--- webpack.config.js
+++ webpack.config.js
@@ -1,91 +1,91 @@
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const FixStyleOnlyEntriesPlugin = require("webpack-fix-style-only-entries");
-const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
-const path = require("path");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const FixStyleOnlyEntriesPlugin = require('webpack-fix-style-only-entries');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const path = require('path');
 
 
-const source = "./scss";  // Absolute or relative path to current directory
-const destination = "./css";  // Absolute or relative path to current directory
+const source = './scss';  // Absolute or relative path to current directory
+const destination = './css';  // Absolute or relative path to current directory
 const entryPoints = [
-    "main",
-    "another",
-    "navbar/style",
-]
+  'main',
+  'another',
+  'navbar/style',
+];
 
 module.exports = (env, argv) => {
 
-    const conf = {
-        entry: entryPoints.reduce((entries, name) => {
-            entries[name] = path.resolve(__dirname, source, `${name}.scss`)
-            return entries
-        }, {}),
-        devtool: false,
-        output: {
-            path: path.resolve(__dirname, destination),
-            filename: "[name].min.js"
-        },
-        stats: {
-            all: false,
-            assets: true,
-            cachedAssets: true,
-            errors: true,
-            errorDetails: true,
-            hash: true,
-            performance: true,
-            publicPath: true,
-            timings: true,
-            warnings: true,
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.s[ca]ss$/,
-                    use: [
-                        {
-                            loader: MiniCssExtractPlugin.loader,
-                            options: {
-                                sourceMap: true,
-                            }
-                        },
-                        {
-                            loader: "css-loader",
-                            options: {
-                                url: false,
-                                sourceMap: true,
-                            }
-                        },
-                        {
-                            loader: "postcss-loader",
-                            options: {
-                                sourceMap: true,
-                                plugins: () => [
-                                    require("autoprefixer")(),
-                                ],
-                            }
-                        },
-                        {
-                            loader: "sass-loader",
-                            options: {
-                                sourceMap: true,
-                            }
-                        }
-                    ]
-                }
-            ]
-        },
-        plugins: [
-            new FixStyleOnlyEntriesPlugin({ silent: true }),
-            new MiniCssExtractPlugin({
-                filename: "./[name].css",
-                chunkFilename: "[id].css"
-            })
-        ],
-        optimization: {
-            minimizer: [
-                new OptimizeCSSAssetsPlugin({})
-            ]
-        },
-    }
+  const conf = {
+    entry: entryPoints.reduce((entries, name) => {
+      entries[name] = path.resolve(__dirname, source, `${name}.scss`);
+      return entries;
+    }, {}),
+    devtool: false,
+    output: {
+      path: path.resolve(__dirname, destination),
+      filename: '[name].min.js'
+    },
+    stats: {
+      all: false,
+      assets: true,
+      cachedAssets: true,
+      errors: true,
+      errorDetails: true,
+      hash: true,
+      performance: true,
+      publicPath: true,
+      timings: true,
+      warnings: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.s[ca]ss$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+                sourceMap: true,
+              }
+            },
+            {
+              loader: 'css-loader',
+              options: {
+                url: false,
+                sourceMap: true,
+              }
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                sourceMap: true,
+                plugins: () => [
+                  require('autoprefixer')(),
+                ],
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true,
+              }
+            }
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new FixStyleOnlyEntriesPlugin({ silent: true }),
+      new MiniCssExtractPlugin({
+        filename: './[name].css',
+        chunkFilename: '[id].css'
+      })
+    ],
+    optimization: {
+      minimizer: [
+        new OptimizeCSSAssetsPlugin({})
+      ]
+    },
+  };
 
-    return conf;
-}
+  return conf;
+};
